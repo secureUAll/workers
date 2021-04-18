ed582c3d2cdaa5edbb2243c6038106364daa9b12
Eduardo Santos <eduardosantoshf@gmail.com>
2021-04-18 17:44:34 +0100

    Script for creating self-signed certificate, putting it on Docker Registry

---
+++ create_cert.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+openssl req -newkey ras:4096 -nodes -sha256 -keyout certs/domain.key -x509 -days 365 -out certs/domain.crt -addext subjectAltName="IP:10.0.2.15" # experimentar, em vez do IP meter um DNS
+
+mkdir -p /etc/docker/certs.d/10.0.2.15
+cp certs/domain.crt /etc/docker/certs.d/10.0.2.15/ca.crt
+
+docker rm -f registry
+
+docker run -d --restart=always --name registry -v "$(pwd)"/certs:/certs -e \
+        -e REGISTRY_HTTP_ADDR=0.0.0.0:443 \
+        -e REGISTRY_HTTP_TLS_CERTIFICATE=/certs/domain.crt \
+        -e REGISTRY_HTTP_TLS_KEY=/certs/domain.key \
+        -p 443:443 \
+        registry:2
