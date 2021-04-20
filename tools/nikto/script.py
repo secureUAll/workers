83b67bcf4ff369edbc112b5165b9ef2a1fe604f9
root <root@DETI-Vuln-Mon.ua.pt>
2021-04-20 18:46:43 +0100

    writing response into file

--- tools/nikto/script.py
+++ tools/nikto/script.py
@@ -1,8 +1,12 @@
 import os
 import subprocess
 
-os.environ['TARGET'] = 'http://hack.me'
+os.environ['TARGET'] = 'hack.me'
 
 os.system("docker pull localhost/nikto:v1")
 
-os.system("docker run --user \"$(id -u):$(id -g)\" localhost/nikto:v1 -h $TARGET -o out.json")
+# os.system("docker run --user \"$(id -u):$(id -g)\" localhost/nikto:v1 -h $TARGET -o response.json")
+
+# os.system("docker run localhost/nikto:v1 -h $TARGET -o response.json")
+
+os.system("docker  run --user \"$(id -u):$(id -g)\" -v `pwd`:`pwd` -w `pwd` -i -t localhost/nikto:v1 pwd -h hack.me -o testinho.json")
