65aee8c99f789210633377983207792329ab5028
root <root@DETI-Vuln-Mon.ua.pt>
2021-04-20 19:10:45 +0100

    working

--- tools/nikto/script.py
+++ tools/nikto/script.py
@@ -2,16 +2,20 @@ import os
 import subprocess
 import argparse
 
-os.system("docker pull localhost/nikto:v1")
-
-os.system("docker  run --user \"$(id -u):$(id -g)\" -v `pwd`:`pwd` -w `pwd` -i -t localhost/nikto:v1 pwd -h {args.arg1} -o {args.arg2}")
+def main(domain, out):
+    print(domain)
+    print(out)
+    os.system("docker pull localhost/nikto:v1")
+    os.system("docker  run --user \"$(id -u):$(id -g)\" -v `pwd`:`pwd` -w `pwd` -i -t localhost/nikto:v1 pwd -h " + domain + " -o " + out)
 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("-h", help="Host to scan")
-    parser.add_argument("-o", help="output file", default="out.json")
+    parser.add_argument("--domain", help="Host to scan")
+    parser.add_argument("--out", help="output file", default="out.json")
 
     args = parser.parse_args()
+
+    main(args.domain, args.out)
 
 
