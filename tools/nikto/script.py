eac94459b8147822deb7b6e3d75b0d7838f24dc6
bastos-01 <p.bastos112@gmail.com>
2021-04-20 18:53:35 +0100

    teste

--- tools/nikto/script.py
+++ tools/nikto/script.py
@@ -1,12 +1,17 @@
 import os
 import subprocess
-
-os.environ['TARGET'] = 'hack.me'
+import argparse
 
 os.system("docker pull localhost/nikto:v1")
 
-# os.system("docker run --user \"$(id -u):$(id -g)\" localhost/nikto:v1 -h $TARGET -o response.json")
+os.system("docker  run --user \"$(id -u):$(id -g)\" -v `pwd`:`pwd` -w `pwd` -i -t localhost/nikto:v1 pwd -h {args.arg1} -o {args.arg2}")
 
-# os.system("docker run localhost/nikto:v1 -h $TARGET -o response.json")
+if __name__ == "__main__":
 
-os.system("docker  run --user \"$(id -u):$(id -g)\" -v `pwd`:`pwd` -w `pwd` -i -t localhost/nikto:v1 pwd -h hack.me -o testinho.json")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-h", help="Host to scan")
+    parser.add_argument("-o", help="output file", default="out.json")
+
+    args = parser.parse_args()
+
+
