46cd4fd9a0f77ae1dea630e043faf1e5848be1f7
bastos-01 <p.bastos112@gmail.com>
2021-04-26 16:32:50 +0100

    script nmap

---
+++ tools/nmap-master/script.py
@@ -0,0 +1,19 @@
+import os
+import subprocess
+import argparse
+
+def main(domain, out):
+    print(domain)
+    print(out)
+    os.system("docker pull localhost/nmap:v1")
+    os.system("docker  run --user \"$(id -u):$(id -g)\" -v `pwd`:`pwd` -w `pwd` -i -t localhost/nmap:v1 pwd " + domain + " -oN " + out)
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--domain", help="Host to scan")
+    parser.add_argument("--out", help="output file", default="out.json")
+
+    args = parser.parse_args()
+
+    main(args.domain, args.out)
