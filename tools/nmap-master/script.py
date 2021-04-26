f21a38733a23c5b0c3f7ba052e0a4b3d71b056a7
root <root@DETI-Vuln-Mon.ua.pt>
2021-04-26 16:53:31 +0100

    nmap working and saving to json file

--- tools/nmap-master/script.py
+++ tools/nmap-master/script.py
@@ -1,12 +1,26 @@
 import os
 import subprocess
 import argparse
+import json
+import xmltodict
 
 def main(domain, out):
     print(domain)
     print(out)
     os.system("docker pull localhost/nmap:v1")
     os.system("docker  run --user \"$(id -u):$(id -g)\" -v `pwd`:`pwd` -w `pwd` -i -t localhost/nmap:v1 " + domain + " -oX " + out)
+
+    convert_to_json(out)
+
+def convert_to_json(output_file):
+
+    f = open(output_file)
+    xml_content = f.read()
+    f.close()
+
+    write_file = open("testinho.json", "w")
+
+    write_file.write(json.dumps(xmltodict.parse(xml_content), indent=4, sort_keys=True))
 
 if __name__ == "__main__":
 
