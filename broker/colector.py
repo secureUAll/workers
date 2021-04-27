a8bcb4eea8635fae4e01fa6d914129f36f84c5a6
bastos-01 <p.bastos112@gmail.com>
2021-04-27 23:45:35 +0100

    working kafka between server and workers

--- broker/colector.py
+++ broker/colector.py
@@ -14,7 +14,7 @@ import time
 
 #logging.basicConfig(level=logging.DEBUG)
 
-producer = KafkaProducer(bootstrap_servers='172.21.0.2:29092',
+producer = KafkaProducer(bootstrap_servers='kafka:9092',
                           security_protocol='SSL',
                           ssl_cafile='./worker_certs/CARoot.pem',
                           ssl_certfile='./worker_certs/certificate.pem',
@@ -23,18 +23,18 @@ producer = KafkaProducer(bootstrap_servers='172.21.0.2:29092',
                           api_version=(2,7,0))
 
 # Write hello world to test topic
-p=producer.send('test', b'Hello World')
+p=producer.send('test', b'ESTA A DAR ZE')
 producer.flush()
 print(p)
-#consumer = KafkaConsumer('test',bootstrap_servers='kafka:9092',
-#                          security_protocol='SSL',
-#                          ssl_cafile='./worker_certs/CARoot.pem',
-#                          ssl_certfile='./worker_certs/certificate.pem',
-#                          ssl_keyfile='./worker_certs/key.pem',
-#                          ssl_check_hostname=False,
-#                          api_version=(2,7,0))
-#for msg in consumer:
-#    print("AAAAAAAAAAAAAAAAAAAAAAAA" +msg)
-#    break
+consumer = KafkaConsumer('test',bootstrap_servers='kafka:9092',
+                          security_protocol='SSL',
+                          ssl_cafile='./worker_certs/CARoot.pem',
+                          ssl_certfile='./worker_certs/certificate.pem',
+                          ssl_keyfile='./worker_certs/key.pem',
+                          ssl_check_hostname=False,
+                          api_version=(2,7,0))
+for msg in consumer:
+    print("AAAAAAAAAAAAAAAAAAAAAAAA" +msg)
+    break
 
 
