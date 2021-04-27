7a49175a021f0ae104b8127040514d7cb24fc2c4
bastos-01 <p.bastos112@gmail.com>
2021-04-27 16:30:07 +0100

    testes broker

---
+++ broker/colector.py
@@ -0,0 +1,37 @@
+from pymongo import MongoClient
+from kafka import KafkaConsumer, KafkaProducer
+from kafka.errors import KafkaError
+import logging
+import time
+
+
+#mongo_client = MongoClient("mongodb://localhost:27017/")
+#print(mongo_client)
+
+
+
+#time.sleep(10)
+
+print("entrou")
+producer = KafkaProducer(bootstrap_servers='kafka:9092',
+                          security_protocol='SSL',
+                          ssl_cafile='./worker_certs/CARoot.pem',
+                          ssl_certfile='./worker_certs/certificate.pem',
+                          ssl_keyfile='./worker_certs/key.pem',
+                          ssl_check_hostname=False,
+                          api_version=(2,7,0))
+
+# Write hello world to test topic
+p=producer.send('test', b'Hello World')
+producer.flush()
+print(p)
+consumer = KafkaConsumer('test',bootstrap_servers='kafka:9092',
+                          security_protocol='SSL',
+                          ssl_cafile='./worker_certs/CARoot.pem',
+                          ssl_certfile='./worker_certs/certificate.pem',
+                          ssl_keyfile='./worker_certs/key.pem',
+                          ssl_check_hostname=False,
+                          api_version=(2,7,0))
+for msg in consumer:
+    print("AAAAAAAAAAAAAAAAAAAAAAAA" +msg)
+    break
