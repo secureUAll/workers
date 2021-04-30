432b0104ab109587df45f9c7be71e098edbcfb2e
Guida <margaridasmartins00@gmail.com>
2021-04-30 22:37:49 +0100

    sasl-kafka

--- broker/colector.py
+++ broker/colector.py
@@ -15,10 +15,13 @@ import time
 #logging.basicConfig(level=logging.DEBUG)
 
 producer = KafkaProducer(bootstrap_servers='kafka:9092',
-                          security_protocol='SSL',
+                          security_protocol='SASL_SSL',
                           ssl_cafile='./worker_certs/CARoot.pem',
                           ssl_certfile='./worker_certs/certificate.pem',
                           ssl_keyfile='./worker_certs/key.pem',
+                          sasl_mechanism='PLAIN',
+                          sasl_plain_username='worker',
+                          sasl_plain_password='worker',
                           ssl_check_hostname=False,
                           api_version=(2,7,0))
 
@@ -27,10 +30,13 @@ p=producer.send('test', b'ESTA A DAR ZE')
 producer.flush()
 print(p)
 consumer = KafkaConsumer('test',bootstrap_servers='kafka:9092',
-                          security_protocol='SSL',
+                          security_protocol='SASL_SSL',
                           ssl_cafile='./worker_certs/CARoot.pem',
                           ssl_certfile='./worker_certs/certificate.pem',
                           ssl_keyfile='./worker_certs/key.pem',
+                          sasl_mechanism='PLAIN',
+                          sasl_plain_username='worker',
+                          sasl_plain_password='worker',
                           ssl_check_hostname=False,
                           api_version=(2,7,0))
 for msg in consumer:
