00e427bff52faa0a9ae2d86bdb368b77f5804c35
bastos-01 <p.bastos112@gmail.com>
2021-05-21 15:52:20 +0100

    added level 1 of scrapping

--- broker/worker.py
+++ broker/worker.py
@@ -12,23 +12,29 @@ import json
 import xmltodict
 
 
-
+# convert xml to json
 def convert_to_json(output_file):
-
     f = open(output_file)
     xml_content = f.read()
     f.close()
-
     #write_file = open("out_json.json", "w")
     #write_file.write(json.dumps(xmltodict.parse(xml_content), indent=4, sort_keys=True))
     return json.dumps(xmltodict.parse(xml_content), indent=4, sort_keys=True)
 
+# read json file
+def read_json_file(output_file):
+    with open(output_file, "r") as f:
+            data = json.load(f)
+            return json.dumps(data, indent=4, sort_keys=True)
 
 
 #time.sleep(30)
-time.sleep(22)
+#time.sleep(22)
+
+# topics from colector
 colector_topics=['INIT','SCAN_REQUEST','LOG']
 
+# golbal variable 
 WORKER_ID = 0
 
 #logging.basicConfig(level=logging.DEBUG)
@@ -62,14 +68,13 @@ consumer = KafkaConsumer(bootstrap_servers='kafka:9092',
                           value_deserializer=lambda m: json.loads(m.decode('latin')),
                           fetch_max_wait_ms=0)
 
+#subscription to the kafka topics
 consumer.subscribe(colector_topics)
 
+# logs
 logging.warning("worker")
 logging.warning(consumer.subscription())
 
-
-#init message 
-random = os.urandom(16)
 
 # Read file with default domains
 domains = []
@@ -80,7 +85,8 @@ with open("domains.txt", "r") as f:
         line = line.strip('\n')
         domains.append(line)
 
-
+#random to init message 
+random = os.urandom(16)
 # message with domains
 message = {'CONFIG':{'ADDRESS_LIST':domains}}
 
@@ -88,48 +94,79 @@ message = {'CONFIG':{'ADDRESS_LIST':domains}}
 producer.send(colector_topics[0], key=random , value=message)
 producer.flush()
 
+# consumer loop
 for message in consumer:
     
     # initial message response to save WORKER_ID
     if message.topic == "INIT":
         # Get ID
         if message.key == random and "WORKER_ID" in message.value:
+            # getting actual Worker ID
             logging.warning(message.value)
             WORKER_ID = message.value['WORKER_ID']
 
+    # scrapping request topic
     elif message.topic== colector_topics[1]:
+        # logs
         logging.warning(int.from_bytes(message.key,"big"))
         logging.warning(WORKER_ID)
+
+        # in case request is to this worker
         if int.from_bytes(message.key,"big") == WORKER_ID:
+
             logging.warning(message.value)
             # get machine to scan
             machine = message.value["MACHINE"]
+            # get scrapping level
+            scrapping_level = int(message.value["SCRAP_LEVEL"])
 
-            # default scrapping value
-            if message.value["SCRAP_LEVEL"] == '2':
+            # level 1
+            if scrapping_level >= 1:
+                # pull image from registry
+                os.system("docker pull localhost:5000/nikto")
+                # run tool
+                os.system("docker run --name=\"nikto_docker\" --user \"$(id -u):$(id -g)\" --volume=`pwd`:`pwd` --workdir=`pwd` -t localhost:5000/nikto -h " + machine + " -o out_nikto.json")
+                #copy file to container
+                os.system("docker cp nikto_docker:/var/temp/out_nikto.json .")
+                #stop and remove containers
+                os.system("docker container stop nikto_docker")
+                os.system("docker container rm nikto_docker")
+
+                #getting json data from file
+                json_nikto = read_json_file("out_nikto.json")
+
+                #sending output
+                logging.warning("vai mandar")
+                producer.send(colector_topics[2], key=bytes([WORKER_ID]), value={"MACHINE":machine, "RESULTS":json_nikto})
+                producer.flush()
+
+            # level 2 (default)
+            if scrapping_level >= 2:
                 # pull image from registry
                 os.system("docker pull localhost:5000/vulscan")
-
-                # runn image
-                os.system("docker  run --name=\"vulscan_docker\" --user \"$(id -u):$(id -g)\" --volume=`pwd`:`pwd` --workdir=`pwd` -t localhost:5000/vulscan -sV --script=vulscan/vulscan.nse " + machine + " -oX out.xml")
+                # runn tool
+                os.system("docker run --name=\"vulscan_docker\" --user \"$(id -u):$(id -g)\" --volume=`pwd`:`pwd` --workdir=`pwd` -t localhost:5000/vulscan -sV --script=vulscan/vulscan.nse " + machine + " -oX out_vulscan.xml")
                 #copy file to container
-                os.system("docker cp vulscan_docker:/var/temp/out.xml .")
+                os.system("docker cp vulscan_docker:/var/temp/out_vulscan.xml .")
                 #stop and remove containers
                 os.system("docker container stop vulscan_docker")
                 os.system("docker container rm vulscan_docker")
 
+                # convert from xml to json
+                output_json = convert_to_json("out_vulscan.xml")
 
-                output_json = convert_to_json("out.xml")
+                logging.warning("vai mandar")
+                producer.send(colector_topics[2], key=bytes([WORKER_ID]), value={"MACHINE":machine, "RESULTS":output_json})
+                producer.flush()
 
-            elif message.value["SCRAP_LEVEL"] == '3':
+            # level 3
+            if scrapping_level >= 3:
                 continue
-            elif message.value["SCRAP_LEVEL"] == '4':
+
+            # level 4
+            if scrapping_level >= 4:
                 continue
-            
-            logging.warning("vai mandar")
-            producer.send(colector_topics[2], key=bytes([WORKER_ID]), value={"MACHINE":machine, "RESULTS":output_json})
-            producer.flush()
-    
+
 #logging.warning(message.topic)
 #logging.warning(message.value)
 
