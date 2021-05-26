a3eff1e8284208fd63bb1a1766a0f01795aaf386
root <root@DETI-Vuln-Mon.ua.pt>
2021-05-26 15:33:24 +0100

    stil not fixed

--- broker/worker.py
+++ broker/worker.py
@@ -123,10 +123,9 @@ def read_json_file(output_file,tool=""):
     with open(output_file, "r") as f:
             #data = json.loads(f.read())
             data = f.read()
-            data= json.loads(data)
-            data["TOOL"] =tool
-            return json.dumps(data, indent=4, sort_keys=True)
-
+            data=json.dumps(data, indent=4, sort_keys=True)
+		data= json.loads(data)
+		data["TOOL"]=tool
 
 #time.sleep(30)
 #time.sleep(22)
